550512c7bf008bf144023e59caae8ba2a17724d2
David Zuber <zuber.david@gmx.de>
2015-03-08 13:24:05 +0000

    Add wrappings for search and top games

--- src/pytwitcher/twitch.py
+++ src/pytwitcher/twitch.py
@@ -2,16 +2,16 @@
 from requests.sessions import Session
 
 
-TWITCH_KRAKENURL = "https://api.twitch.tv/kraken/"
+TWITCH_KRAKENURL = 'https://api.twitch.tv/kraken/'
 """The baseurl for the twitch api"""
 
-TWITCH_HEADER_ACCEPT = "application/vnd.twitchtv.v3+json"
+TWITCH_HEADER_ACCEPT = 'application/vnd.twitchtv.v3+json'
 """The header for the ``Accept`` key to tell twitch which api version it should use"""
 
-TWITCH_USHERURL = "http://usher.twitch.tv/api/"
+TWITCH_USHERURL = 'http://usher.twitch.tv/api/'
 """The baseurl for the twitch usher api"""
 
-TWITCH_APIURL = "http://api.twitch.tv/api/"
+TWITCH_APIURL = 'http://api.twitch.tv/api/'
 """The baseurl for the old twitch api"""
 
 
@@ -38,7 +38,7 @@ class BaseSession(Session):
         :rtype: :class:`BaseSession`
         :raises: None
         """
-        if "_instance" not in cls.__dict__ or not cls._instance:
+        if '_instance' not in cls.__dict__ or not cls._instance:
             cls._instance = cls()
         return cls._instance
 
@@ -71,7 +71,7 @@ class KrakenSession(BaseSession):
         :raises: None
         """
         super(KrakenSession, self).__init__(baseurl=TWITCH_KRAKENURL)
-        self.headers.update({"Accept": TWITCH_HEADER_ACCEPT})
+        self.headers.update({'Accept': TWITCH_HEADER_ACCEPT})
 
 
 class UsherSession(BaseSession):
@@ -106,13 +106,75 @@ class APISession(BaseSession):
         :rtype: (:class:`unicode`, :class:`unicode`)
         :raises: None
         """
-        r = self.get("channels/%s/access_token" % channel).json()
-        return r["token"], r["sig"]
+        r = self.get('channels/%s/access_token' % channel).json()
+        return r['token'], r['sig']
 
 
 class Game(object):
     """Game on twitch.tv
     """
+
+    @classmethod
+    def wrap_search(cls, response):
+        """Wrap the response from a game search into instances
+        and return them
+
+        :param response: The response from searching a game
+        :type response: :class:`requests.models.Response`
+        :returns: the new game instances
+        :rtype: :class:`Game`
+        :raises: None
+        """
+        games = []
+        json = response.json()
+        gamejsons = json['games']
+        for j in gamejsons:
+            g = cls.wrap_json(j)
+            games.append(g)
+        return games
+
+    @classmethod
+    def wrap_topgames(cls, response):
+        """Wrap the response from quering the top games into instances
+        and return them
+
+        :param response: The response for quering the top games
+        :type response: :class:`requests.models.Response`
+        :returns: the new game instances
+        :rtype: :class:`Game`
+        :raises: None
+        """
+        games = []
+        json = response.json()
+        topjsons = json['top']
+        for t in topjsons:
+            g = cls.wrap_json(json=t['game'],
+                              viewers=t['viewers'],
+                              channels=t['channels'])
+            games.append(g)
+        return games
+
+    @classmethod
+    def wrap_json(cls, json, viewers=None, channels=None):
+        """Create a Game instance for the given json
+
+        :param json: the dict with the information of the game
+        :type json: :class:`dict`
+        :param viewers: The viewer count
+        :type viewers: :class:`int`
+        :param channels: The viewer count
+        :type channels: :class:`int`
+        :returns: the new game instance
+        :rtype: :class:`Game`
+        :raises: None
+        """
+        g = Game(name=json['name'],
+                 box=json['box'],
+                 logo=json['logo'],
+                 twitchid=json['_id'],
+                 viewers=viewers,
+                 channels=channels)
+        return g
 
     def __init__(self, name, box, logo, twitchid, viewers=None, channels=None):
         """Initialize a new game
@@ -156,6 +218,6 @@ class Game(object):
         :raises: None
         """
         ks = KrakenSession.get_instance()
-        r = ks.get("streams/summary", params={"game": self.name}).json()
-        self.viewers = r["viewers"]
-        self.channels = r["channels"]
+        r = ks.get('streams/summary', params={'game': self.name}).json()
+        self.viewers = r['viewers']
+        self.channels = r['channels']
--- test/test_twitch_game.py
+++ test/test_twitch_game.py
@@ -5,19 +5,63 @@ from pytwitcher import twitch
 
 
 @pytest.fixture(scope="function")
-def mock_session(monkeypatch):
+def mock_session_get_viewers(monkeypatch):
     monkeypatch.setattr(twitch.BaseSession, "get", mock.Mock())
-
-
-def test_game_fetch_viewers(mock_session):
-    bs = twitch.BaseSession()
     mockjson = {"viewers": 124,
                 "channels": 12}
     mockresponse = mock.Mock()
     mockresponse.json.return_value = mockjson
-    bs.get.return_value = mockresponse
+    twitch.BaseSession.get.return_value = mockresponse
 
+
+def test_game_fetch_viewers(mock_session_get_viewers):
+    bs = twitch.BaseSession()
     game = twitch.Game(name="Test", box={}, logo={}, twitchid=214)
     assert game.viewers == 124
     assert game.channels == 12
     bs.get.assert_called_with("streams/summary", params={"game": "Test"})
+
+
+@pytest.fixture(scope="function")
+def game1json():
+    return {"name": "Test", "box": {}, "logo": {}, "_id": 1252}
+
+
+@pytest.fixture(scope="function")
+def game2json():
+    return {"name": "Test2", "box": {}, "logo": {}, "_id": 1212}
+
+
+def assert_game_equals_json(game, json):
+    assert game.name == json["name"]
+    assert game.box == json["box"]
+    assert game.logo == json["logo"]
+    assert game.twitchid == json["_id"]
+
+
+def test_wrap_json(game1json, mock_session_get_viewers):
+    g = twitch.Game.wrap_json(game1json)
+    assert_game_equals_json(g, game1json)
+
+
+def test_wrap_search(game1json, game2json, mock_session_get_viewers):
+    searchjson = {"games": [game1json, game2json]}
+    mockresponse = mock.Mock()
+    mockresponse.json.return_value = searchjson
+    games = twitch.Game.wrap_search(mockresponse)
+    for g, j  in zip(games, [game1json, game2json]):
+        assert_game_equals_json(g, j)
+
+
+def test_wrap_topgames(game1json, game2json):
+    topjson = {"top": [{"game": game1json, "viewers": 123, "channels": 10},
+                       {"game": game2json, "viewers": 543, "channels": 42}]}
+    mockresponse = mock.Mock()
+    mockresponse.json.return_value = topjson
+    games = twitch.Game.wrap_topgames(mockresponse)
+    for g, j in zip(games, [game1json, game2json]):
+        assert_game_equals_json(g, j)
+    assert games[0].viewers == 123
+    assert games[0].channels == 10
+    assert games[1].viewers == 543
+    assert games[1].channels == 42
