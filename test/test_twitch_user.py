083136691447851a9a6752166e43130bf587b1e4
David Zuber <zuber.david@gmx.de>
2015-03-09 11:28:23 +0000

    Add tests for user

---
+++ test/test_twitch_user.py
@@ -0,0 +1,12 @@
+from pytwitcher import twitch
+import conftest
+
+
+def test_wrap_json(user1json):
+    s = twitch.User.wrap_json(user1json)
+    conftest.assert_user_equals_json(s, user1json)
+
+
+def test_wrap_get_user(get_user_response, user1json):
+    s = twitch.User.wrap_get_user(get_user_response)
+    conftest.assert_user_equals_json(s, user1json)
