cbb99636f31b2c43ae855caee5b2372c554f2a1d
David Zuber <zuber.david@gmx.de>
2015-03-30 23:19:03 +0200

    Add more tests and comments

--- test/test_session_qttwitchsession.py
+++ test/test_session_qttwitchsession.py
@@ -2,7 +2,6 @@ import mock
 import pytest
 
 from test import conftest
-from pytwitcher import session
 from pytwitcherapi import session as apisession
 
 
@@ -16,11 +15,21 @@ def apistreams(apistream1):
     return [apistream1]
 
 
+@pytest.fixture(scope='function')
+def apichannels(apichannel1):
+    return [apichannel1]
+
+
 def pytest_generate_tests(metafunc):
     argnames = ['method', 'kwargs', 'returnvfixture', 'assertfunc']
-    if not set(argnames).issubset(metafunc.funcargnames):
+    if metafunc.function.__name__ != 'test_qtmodels':
         return
     args = []
+    # for each arg you append you test:
+    # 1. a method
+    # 2. with the given args
+    # 3. whether it returns qtmodels that resemble the given fixture
+    # 4. and check it with the given function
     args.append(('search_games',
                  {'query': 'THC', 'live': False},
                  'apigames',
@@ -34,19 +43,35 @@ def pytest_generate_tests(metafunc):
                   'limit': 12, 'offset': 24},
                  'apistreams',
                  conftest.assert_stream_eq_apistream))
+    args.append(('search_streams',
+                 {'query': 'streams?', 'hls': True,
+                  'limit': 11, 'offset': 23},
+                 'apistreams',
+                 conftest.assert_stream_eq_apistream))
+    args.append(('search_channels',
+                 {'query': 'Haha', 'limit': 12, 'offset': 2},
+                 'apichannels',
+                 conftest.assert_channel_eq_apichannel))
     metafunc.parametrize(argnames, args)
 
 
 def test_qtmodels(method, kwargs, returnvfixture, assertfunc,
                monkeypatch, mockedsession, request):
+    # return the value of the given fixture for the mock
     returnv = request.getfuncargvalue(returnvfixture)
     m = mock.Mock()
     m.return_value = returnv
+    # mock the method on the api class
     monkeypatch.setattr(apisession.TwitchSession, method, m)
+    # call the new function which should return qtmodels
     rvalues = getattr(mockedsession, method)(**kwargs)
+    # assert that the list is not empty
     assert rvalues
+    # for each returned value check if they resemble the one of the fixture
     for qtm, apim in zip(rvalues, returnv):
+        # use the assert function
         assertfunc(qtm, apim)
         assert qtm.session is mockedsession
         assert qtm.cache is mockedsession.cache
+    # check if the call was made correctly
     m.assert_called_with(**kwargs)
