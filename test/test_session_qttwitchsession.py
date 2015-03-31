1e8fc35aaae30bcfc28ab8c39424e50828b1e423
David Zuber <zuber.david@gmx.de>
2015-03-31 14:55:03 +0200

    Assert what happens if None is returned

--- test/test_session_qttwitchsession.py
+++ test/test_session_qttwitchsession.py
@@ -22,7 +22,13 @@ def apichannels(apichannel1):
     return [apichannel1]
 
 
+@pytest.fixture(scope='function')
+def none():
+    return None
+
+
 def assertlist(qtmodels, apimodels, assertfunc):
+    assert qtmodels
     for qtm, apim in zip(qtmodels, apimodels):
         # use the assert function
         assertfunc(qtm, apim)
@@ -30,6 +36,11 @@ def assertlist(qtmodels, apimodels, assertfunc):
 
 def assertsingle(qtmodel, apimodel, assertfunc):
     assertfunc(qtmodel, apimodel)
+
+
+def assertnone(qtmodel, apimodel):
+    assert qtmodel is None
+    assert apimodel is None
 
 
 def pytest_generate_tests(metafunc):
@@ -58,44 +69,36 @@ def pytest_generate_tests(metafunc):
     # 2. with the given args
     # 3. whether it returns qtmodels that resemble the given fixture
     # 4. and check it with the given function
-    args.append(('search_games',
-                 {'query': 'THC', 'live': False},
-                 'apigames',
-                 assertlistgame))
-    args.append(('top_games',
-                 {'limit': 20, 'offset': 100},
-                 'apigames',
-                 assertlistgame))
+    args.append(('search_games', {'query': 'THC', 'live': False},
+                 'apigames', assertlistgame))
+    args.append(('top_games', {'limit': 20, 'offset': 100},
+                 'apigames', assertlistgame))
     args.append(('get_streams',
                  {'game': 'Test', 'channels': ['c1', 'c2'],
                   'limit': 12, 'offset': 24},
-                 'apistreams',
-                 assertliststream))
+                 'apistreams', assertliststream))
     args.append(('search_streams',
                  {'query': 'streams?', 'hls': True,
                   'limit': 11, 'offset': 23},
-                 'apistreams',
-                 assertliststream))
-    args.append(('search_channels',
-                 {'query': 'Haha', 'limit': 12, 'offset': 2},
-                 'apichannels',
-                 assertlistchannel))
-    args.append(('get_game',
-                 {'name': 'somegame'},
-                 'apigame1',
-                 assertsinglegame))
-    args.append(('get_channel',
-                 {'name': 'somechannel'},
-                 'apichannel1',
-                 assertsinglechannel))
-    args.append(('get_stream',
-                 {'channel': 'somechannel'},
-                 'apistream1',
-                 assertsinglestream))
-    args.append(('get_user',
-                 {'name': 'someuser'},
-                 'apiuser1',
-                 assertsingleuser))
+                 'apistreams', assertliststream))
+    args.append(('search_channels', {'query': 'Haha', 'limit': 12, 'offset': 2},
+                 'apichannels', assertlistchannel))
+    args.append(('get_game', {'name': 'somegame'},
+                 'apigame1', assertsinglegame))
+    args.append(('get_channel', {'name': 'somechannel'},
+                 'apichannel1', assertsinglechannel))
+    args.append(('get_stream', {'channel': 'somechannel'},
+                 'apistream1', assertsinglestream))
+    args.append(('get_user', {'name': 'someuser'},
+                 'apiuser1', assertsingleuser))
+    args.append(('get_game', {'name': 'somegame'},
+                 'none', assertnone))
+    args.append(('get_channel', {'name': 'somechannel'},
+                 'none', assertnone))
+    args.append(('get_stream', {'channel': 'somechannel'},
+                 'none', assertnone))
+    args.append(('get_user', {'name': 'someuser'},
+                 'none', assertnone))
 
     metafunc.parametrize(argnames, args)
 
@@ -110,8 +113,6 @@ def test_qtmodels(method, kwargs, returnvfixture, assertfunc,
     monkeypatch.setattr(apisession.TwitchSession, method, m)
     # call the new function which should return qtmodels
     rvalues = getattr(mockedsession, method)(**kwargs)
-    # assert that the list is not empty
-    assert rvalues
     # for each returned value check if they resemble the one of the fixture
     assertfunc(rvalues, returnv)
     # check if the call was made correctly
