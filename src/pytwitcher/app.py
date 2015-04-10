c12ef676c211b68023582fd8e378f07f67cfe775
David Zuber <zuber.david@gmx.de>
2015-04-10 14:05:59 +0200

    Use tray-icon instead of main window

--- src/pytwitcher/app.py
+++ src/pytwitcher/app.py
@@ -17,15 +17,20 @@ class PyTwitcherApp(object):
         """
         super(PyTwitcherApp, self).__init__()
         self.qapp = QtGui.QApplication.instance() or QtGui.QApplication([])
+        self.qapp.setQuitOnLastWindowClosed(False)
         self.session = session.QtTwitchSession()
+        self.mainmenu = QtGui.QMenu("PyTwitcher")
         self.menu = QtGui.QMenu("Streams")
-        self.main = QtGui.QMainWindow()
+        self.mainmenu.addMenu(self.menu)
+        self.quitaction = QtGui.QAction("Quit", self.mainmenu)
+        self.quitaction.triggered.connect(self.quit_app)
+        self.mainmenu.addAction(self.quitaction)
 
         self.data = cache.DataRefresher(300000)
         self.data.add_refresher('top_games', self.session.top_games)
         self.data.refresh_ended.connect(self.update_menu)
-        self.main.menuBar().setNativeMenuBar(False)
-        self.main.menuBar().addMenu(self.menu)
+        self.tray = QtGui.QSystemTrayIcon()
+        self.tray.setContextMenu(self.mainmenu)
 
     def launch(self, gui=True):
         """
@@ -38,11 +43,20 @@ class PyTwitcherApp(object):
         """
         self.data.refresh_all()
         self.data.start()
-        self.main.show()
+        self.tray.setVisible(True)
         if gui is True:
             return self.qapp.exec_()
         else:
             return
+
+    def quit_app(self, ):
+        """
+
+        :returns: None
+        :rtype: None
+        :raises: None
+        """
+        self.qapp.quit()
 
     def update_menu(self, name):
         """
@@ -73,7 +87,7 @@ class PyTwitcherApp(object):
         :rtype: None
         :raises: None
         """
-        quali, ok = QtGui.QInputDialog.getItem(self.main, "Choose Quality",
+        quali, ok = QtGui.QInputDialog.getItem(None, "Choose Quality",
                                                "Quality Options for %s" %
                                                stream.channel.name,
                                                stream.quality_options,
