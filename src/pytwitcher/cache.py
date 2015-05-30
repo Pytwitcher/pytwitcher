87ef5862fc3985d309654a0f78cd6e33d4313cf6
David Zuber <zuber.david@gmx.de>
2015-05-30 20:36:20 +0200

    Pixmaploader makes use of defaultdict

--- src/pytwitcher/cache.py
+++ src/pytwitcher/cache.py
@@ -1,11 +1,22 @@
 """This module contains classes for caching data"""
+import collections
 import types
 
 from PySide import QtGui, QtCore
 
 
-class PixmapLoader(dict):
-    """A dict like object that can loads and stores pixmaps
+class Defaultdict(collections.defaultdict):
+    """A defaultdict wich passes key to the default factory."""
+    def __missing__(self, key):
+        if self.default_factory is None:
+            raise KeyError(key)
+        else:
+            ret = self[key] = self.default_factory(key)
+            return ret
+
+
+class PixmapLoader(Defaultdict):
+    """A dict like object that loads and stores pixmaps
 
     Keys are urls to pictures. Values are :class:`QtGui.QPixmap`.
     If the key is not in the dict, the picture will be downloaded,
@@ -20,29 +31,8 @@ class PixmapLoader(dict):
         :type session: :class:`requests.Session`
         :raises: None
         """
-        super(PixmapLoader, self).__init__()
+        super(PixmapLoader, self).__init__(self.load_picture)
         self.session = session
-
-    def __getitem__(self, key):
-        """Retrieve a :class:`QtGui.QPixmap` for the given key.
-
-        If the url has not been cached yet, the picture will be downloaded
-        and stored.
-
-        :param key: url to a picture
-        :type key: :class:`str`
-        :returns: the pixmap for the given url
-        :rtype: :class:`QtGui.QPixmap`
-        :raises: :class:`requests.HTTPError` if the picture cannot be downloaded.
-        """
-        if key in self:
-            pixmap = super(PixmapLoader, self).get(key)
-            return pixmap
-
-        # download pixmap
-        pixmap = self.load_picture(key)
-        self[key] = pixmap
-        return pixmap
 
     def load_picture(self, url):
         """Load the picutre at the given url
